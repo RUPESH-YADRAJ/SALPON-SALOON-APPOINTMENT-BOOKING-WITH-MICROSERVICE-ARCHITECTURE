e7b92a7c14c122a15db525059c9b15ae0b4db88e
Ranjit <ranjit.221736@ncit.edu.np>
2025-06-20 22:36:31 +0545

    Image updated

--- src/Customer/saloon/Saloon Details/SaloonDetail.jsx
+++ src/Customer/saloon/Saloon Details/SaloonDetail.jsx
@@ -6,10 +6,10 @@ const SaloonDetail = () => {
         <div className='space-y-5 mb-20'>
             <section className="grid grid-cols-2 gap-3">
                 <div className='col-span-2'>
-                    <img className='w-full rounded-md h-[15rem] object-cover' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTB9aMTuhmD9Q11l2PKy8MyzYyjsfbDgKq_rA&s" alt="" />
+                    <img className='w-full rounded-md h-[15rem] object-cover' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpUrfawq7Z2hOHDGzcpFJSHT1cDQpH__TUOw&s" alt="" />
                 </div>
                 <div className='col-span-1'>
-                    <img className='w-full rounded-md h-[15rem] object-cover' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpUrfawq7Z2hOHDGzcpFJSHT1cDQpH__TUOw&s" alt="" />
+                    <img className='w-full rounded-md h-[15rem] object-cover' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRx0RIppj5aIC1KRh2aerPBmj-dv5CwiJzlcQ&s" alt="" />
                 </div>
                 <div className='col-span-1'>
                     <img className='w-full rounded-md h-[15rem] object-cover' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKsLHcbrOS59g40FR8HQIBqKLxtkveEVxj9A&s" alt="" />
