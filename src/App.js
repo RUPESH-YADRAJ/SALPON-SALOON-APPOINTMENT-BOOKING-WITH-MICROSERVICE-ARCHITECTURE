d5f62e79e66446b2a2ade4b6bd9ce4df22fdbcbb
Ranjit <ranjit.221736@ncit.edu.np>
2025-06-23 22:39:59 +0545

    updated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { Button, ThemeProvider } from '@mui/material';
 import greenTheme from './theme/greenTheme';
 import Home from './Customer/Home/Home';
-import SaloonDetails from './Customer/Saloon/Saloon Details/SaloonDetails';
+import SaloonDetails from './Customer/saloon/Saloon Details/SaloonDetails';
 import Bookings from './Customer/Booking/Bookings';
 import Notifications from './Customer/Notification/Notifications';
 import Navbar from './Customer/Navbar/Navbar';
