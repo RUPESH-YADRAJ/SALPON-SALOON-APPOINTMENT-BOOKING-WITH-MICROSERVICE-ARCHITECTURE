edd71ada4891e10982d7d6e7b5ec3877b9f0eac9
Ranjit <ranjit.221736@ncit.edu.np>
2025-06-30 20:58:00 +0545

    Image updated

--- src/Customer/saloon/SaloonCard.jsx
+++ src/Customer/saloon/SaloonCard.jsx
@@ -6,7 +6,7 @@ const SaloonCard = () => {
     return (
         <div onClick={() => navigate("/saloon/2")} className='cursor-pointer' >
             <div className='w-56 md:w-80 rounded-md bg-slate-100'>
-                <img className='w-full h-[15rem] object-cover rounded-t-md' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnNPGFH1HxImZ7mOj_nCXej9pUcTyglWQ8Yw&s" alt="" />
+                <img className='w-full h-[15rem] object-cover rounded-t-md' src="https://cdn.pixabay.com/photo/2022/01/24/20/03/salon-6964546_1280.jpg" alt="" />
                 <div className='p-5 space-y-2'>
                     <h1>Pablo Saloon</h1>
                     <div className='text-white text-sm p-1 bg-green-700 rounded-full w-14 flex items-center justify-center gap-1'>
